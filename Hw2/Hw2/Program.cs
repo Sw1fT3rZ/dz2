377c25ef22c509ddad2bcadf0f111d3d9d0c67ba
Sw1fT3rZ <130749643+Sw1fT3rZ@users.noreply.github.com>
2024-03-07 00:15:36 +0200

    Add files via upload

---
+++ Hw2/Hw2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 20;
+        int[] A = new int[N];
+        List<int> B = new List<int>();
+        int countInRange = 0;
+
+        Random rand = new Random();
+
+        Console.WriteLine("Array A:");
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = rand.Next(-200, 200);
+            Console.Write(A[i] + " ");
+
+            if (A[i] >= -100 && A[i] <= 100)
+            {
+                countInRange++;
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Number of elements in the range -100 to +100: " + countInRange);
+
+        for (int i = 0; i < N; i++)
+        {
+            if (A[i] <= 888)
+            {
+                B.Add(A[i]);
+            }
+        }
+
+        B.Sort((a, b) => b.CompareTo(a));
+
+        Console.WriteLine("Array B after sorting in descending order:");
+        foreach (int element in B)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine("\nPress any key to exit.");
+        Console.ReadKey();
+    }
+}
